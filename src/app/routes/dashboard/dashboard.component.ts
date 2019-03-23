80ef0cef8e48a7c85d8adef057b232f1f818b39d
xuyangzhou <1367685484@qq.com>
2019-03-23 18:51:00 +0800

    three merge

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -8,7 +8,7 @@ import { Subject } from 'rxjs';
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
-  tabs = [ 
+  tabs = [
     {
       title: '健康状态评估'
     },
@@ -32,7 +32,7 @@ export class DashboardComponent implements OnInit {
     {
       name: 'B列气缸排温',
       unit: '（℃）',
-      analog: 92
+      analog: 90
     },
     {
       name: 'C列气缸排温',
@@ -45,12 +45,57 @@ export class DashboardComponent implements OnInit {
       analog: 33
     }
   ]
-  
+
   constructor(
     private http: _HttpClient
   ) { }
 
   ngOnInit() {
+    
+    for(let i = 0;i< this.list.length; i++){
+      console.log(this.fnReturnState(this.list[i].analog))
+    }
   }
 
+  fnReturnState(num: any) {
+    let state: string = '';
+    if(num >= 0 && num < 0.8){
+      state = 'danger';
+    }else if(num >= 0.8 && num < 24.3){
+      state = 'warn';
+    }else if(num >= 24.3 && num < 57.2){
+      state = 'normal';
+    }else if(num >= 57.2 && num < 80.8){
+      state = 'warn';
+    }else if(num >= 80.8 && num <= 100){
+      state = 'danger';
+    }else{
+      state = 'unnormal';
+    }
+    return {
+      state: state,
+      style: {
+        bottom: num + "%"
+      }
+    };
+    // switch (num) {
+    //   case num >= 80.8:
+    //     state = 'danger';
+    //     break;
+    //   case num >= 57.2 && num < 80.8:
+    //     state = 'warn';
+    //     break;
+    //   case num >= 24.3 && num < 57.2:
+    //     state = 'nomarl';
+    //     break;
+    //   case num >= 0.8 && num < 24.3:
+    //     state = 'warn';
+    //     break;
+    //   case num >= 0 && num < 0.8:
+    //     state = 'danger';
+    //     break;
+    //   default:
+    //     state = 'unnomarl';
+    // }
+  };
 }
